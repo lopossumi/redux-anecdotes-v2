6af621a7c9a86ac7762acfbc71ea05cdbffd6b26
Mikko Loponen <mikko.loponen@iki.fi>
2018-02-23 11:18:34 +0200

    6.10 json-server initialization

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
+import { connect } from 'react-redux'
+import { initialize } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
+
 class App extends React.Component {
+  componentDidMount = async () => {
+    const notes = await anecdoteService.getAll()
+    this.props.initialize(notes)
+  }
 
   render() {
     return (
@@ -17,4 +25,9 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default connect(
+  null,
+  { initialize }
+)(App)
+
+//export default App
